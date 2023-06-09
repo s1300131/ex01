e445362816a94f2edc86d498e88543f36955d0f9
s1300131 <s1300131@u-aizu.ac.jp>
2023-06-09 18:13:16 +0900

    Won/lost message modified

--- DiceGame.java
+++ DiceGame.java
@@ -5,15 +5,17 @@ class DiceGame {
 	int[] a = new int[2];
 	int total;
 	a[0] = (int)Math.ceil(Math.random()*6)%7;
-	a[1] = (int)Math.ceil(Math.random()*6);
+	a[1] = (int)Math.ceil(Math.random()*6)%7;
 	total = a[0]+a[1];
 	System.out.println("What is your name?");
-	System.out.print(">");
+	System.out.print("> ");
 	String name = sc.next();
 	System.out.println("Hello, " + name + "!");
-	System.out.println("Rolling dice...");
+	System.out.println("Rolling the dice...");
 	for(int i=1;i<3;i++)
 	    System.out.println("Die "+ i +": " + a[i-1]);
 	System.out.println("Total value: " + total);
+	if(total>7) System.out.println(name+" won!");
+	else System.out.println(name+" lost!");
     }
 }
